00c777025ea18218fba3dffdb9db5386f1842933
PoojaRamaraj <119429335+Poojaram18@users.noreply.github.com>
2023-02-18 19:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+var table = document.querySelector('table');
+ var lastRow = document.createElement('tr');
+ var prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ var child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+ 
 
-const getSum = () => {
-//Add your code here
-  
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
